15372fc976a31687e7e3c39b3d1116f0c7396211
Berkay Güntürk <78702311+BerkayGunturk@users.noreply.github.com>
2023-09-08 21:19:27 +0300

    main

--- Core/Models/Account.cs
+++ Core/Models/Account.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +14,35 @@ namespace Core.Models
         public string SiteUrl { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+
+
+        
+
+        public void SetPassword(string password)
+        {
+            // Parolayı güvenli bir şekilde şifrele
+            Password = EncryptPassword(password);
+        }
+
+        public bool VerifyPassword(string password)
+        {
+            // Kullanıcının girdiği parolayı şifreleyin ve saklanan parola ile karşılaştırın
+            string inputPasswordHash = EncryptPassword(password);
+            return Password == inputPasswordHash;
+        }
+
+        private string EncryptPassword(string password)
+        {
+            // Parolayı güvenli bir şekilde şifreleyin (örneğin, SHA256 kullanabilirsiniz)
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
     }
+
 }
+
+
